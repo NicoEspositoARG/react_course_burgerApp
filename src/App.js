2e6378b6bf028c5977dc243221ea9a440492a5ff
Nico Esposito <nico.esposito@gmail.com>
2019-06-26 17:55:25 -0300

    completed 252

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Layout from './hoc/Layout/Layout'
+import { Route, Switch } from 'react-router-dom';
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 function App() {
   return (
-    <div >
+    <div>
       <Layout>
-        <BurgerBuilder/>
-      </Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+    </Layout>
     </div>
   );
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,31 +96,42 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Nico Esposito',
-        address: {
-          street: "Test Street 1",
-          zipCode: "41531",
-          country: "Uruguay"
-          },
-        email: "test@test.com",  
-        },
-        deliveryMethod: "cheapest"
+    const queryParams = [];
+    for (let i in this.state.ingredients){
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
     }
-    axios.post('/orders.json', order)
-    .then(
-      response => { 
-        this.setState({ loading:false, purchasing:false })        
-      })
-    .catch(error => {
-      this.setState({ loading:false, purchasing:false })        
-      console.log(error)
+    const queryString = queryParams.join('&');
+
+    
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
     })
+    // this.setState({ loading: true });
+
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   customer: {
+    //     name: 'Nico Esposito',
+    //     address: {
+    //       street: "Test Street 1",
+    //       zipCode: "41531",
+    //       country: "Uruguay"
+    //       },
+    //     email: "test@test.com",  
+    //     },
+    //     deliveryMethod: "cheapest"
+    // }
+    // axios.post('/orders.json', order)
+    // .then(
+    //   response => { 
+    //     this.setState({ loading:false, purchasing:false })        
+    //   })
+    // .catch(error => {
+    //   this.setState({ loading:false, purchasing:false })        
+    //   console.log(error)
+    // })
   }
 
   render() {
