696a458c88e623000693104d56e5b0d7f48d2c01
Nico Esposito <nico.esposito@gmail.com>
2019-08-15 16:17:06 -0300

    completed 362

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,19 +12,28 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
-      <p> Current Price: <strong> {props.price.toFixed(2)} </strong></p>
+    <p>
+      {" "}
+      Current Price: <strong> {props.price.toFixed(2)} </strong>
+    </p>
 
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
-        added={()=> props.ingridientAdded(ctrl.type)}
+        added={() => props.ingridientAdded(ctrl.type)}
         removed={() => props.ingridientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]}
       />
     ))}
 
-    <button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordered} >ORDER NOW </button>
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}
+    >
+      {props.isAuth ? "ORDER NOW" : "SIGN UP TO ORDER"}
+    </button>
   </div>
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -75,7 +75,12 @@ class BurgerBuilder extends Component {
   // }
 
   purchaseHandler = () => {
-    this.setState({purchasing:true})
+    if (this.props.isAuthenticaded) {
+      this.setState({purchasing:true})
+    } else {
+      this.props.onSetAuthRedirectPath("/checkout")
+      this.props.history.push("/auth");
+    }
   }
 
   purchaseCancelHandler = () => {
@@ -111,6 +116,7 @@ class BurgerBuilder extends Component {
             price={this.props.price}
             purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticaded}
           />
         </Aux>)
       orderSummary = (
@@ -146,7 +152,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticaded: state.auth.token !== null
   }
 }
 
@@ -157,7 +164,8 @@ const mapDispatchToProps = dispatch => {
 
     onIngredientRemoved: ingName => dispatch( actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
 
     };
 };
